b0ceb8bfb39a5cecae1405c702f5c71c17f3ab55
svitlyi <svitlyi_a@itstep.academy>
2024-12-12 18:08:49 +0200

    Add project files.

---
+++ CSharp_P33/game.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel;
+
+namespace Game
+{
+    
+    class Character
+    {
+        string? name;
+        int health;
+        int damage;
+        int defence;
+
+
+        public int Health
+        {
+            get { return this.health; }
+            set { this.health = Math.Max(value, 0); }
+        }
+
+        public Character(string name, int health, int damage, int defence) { 
+            this.name = name;
+            this.health = health;
+            this.damage = damage;
+            this.defence = defence;
+        }
+        
+        public Character() {
+            this.name = "Jonny";
+            this.health = 100;
+            this.damage = 5;
+            this.defence = 0;
+        }
+
+        public int GetHealth()
+        {
+            return this.health;
+        }
+        public void SetHealth(int health)
+        {
+            this.health = Math.Max(health, 0);
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($" -< {this.name} >-");
+            Console.WriteLine($" Здоров\'я: {this.health}");
+            Console.WriteLine($" Шкода: {this.damage}");
+            Console.WriteLine($" Захист: {this.defence}");
+        }
+
+        public int TakeDamage( int damage )
+        {
+            this.health = Math.Max(this.health - damage, 0);
+            return this.health;
+        }
+
+        public int Attack(Character target)
+        {
+            Random rnd = new Random(Convert.ToInt32(DateTimeOffset.Now.ToUnixTimeSeconds()));
+            int final_damage = Math.Max(this.damage + rnd.Next(-3, 3), 0);
+            target.TakeDamage(final_damage);
+            return final_damage;
+        }
+        
+        public bool IsAlive()
+        {
+            return this.health > 0;
+        }
+    }
+}
---
+++ CSharp_P33/program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using Game;
+
+Console.OutputEncoding = UTF8Encoding.UTF8;
+Console.InputEncoding = UTF8Encoding.UTF8;
+
+Character player1 = new Character();
+player1.Print();
+Console.WriteLine();
+
+
+Character player2 = new Character("James", 90, 8, 0);
+player2.Print();
+
+while (player1.IsAlive() && player2.IsAlive())
+{
+    player1.Attack(player2);
+    player2.Attack(player1);
+}
+
+player1.GetHealth();
+player1.SetHealth(-100);
+player1.Health = -100;
+Console.WriteLine(player1.Health);
+
+Console.WriteLine("\n");
+player1.Print();
+player2.Print();
+
+int number = Convert.ToInt32(Console.ReadLine());
+
